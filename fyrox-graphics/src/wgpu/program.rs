bc6db0296919f3c4b7a6cd95ecddf4f8ff9c7cb8
Tom Benham <tom.benham13@gmail.com>
2025-02-26 17:13:12 +0100

    WIP

--- fyrox-graphics/src/wgpu/program.rs
+++ fyrox-graphics/src/wgpu/program.rs
@@ -1,6 +1,10 @@
+use fyrox_core::log::Log;
+
 use crate::{
     error::FrameworkError,
-    gpu_program::{GpuProgramTrait, ShaderResourceDefinition, ShaderResourceKind},
+    gpu_program::{
+        GpuProgramTrait, ShaderPropertyKind, ShaderResourceDefinition, ShaderResourceKind,
+    },
 };
 
 use super::server::WgpuGraphicsServer;
@@ -17,76 +21,101 @@ impl WgpuProgram {
         fragment_source: &str,
         resources: &[ShaderResourceDefinition],
     ) -> Result<WgpuProgram, FrameworkError> {
-        let bind_group_layouts: Vec<wgpu::BindGroupLayout> = resources
-            .into_iter()
-            .map(|resource| {
-                let bind_group_layout =
-                    server
-                        .device
-                        .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-                            label: Some(&(resource.name.to_mutable() + " BindGroupLayout")),
-                            entries: match resource.kind {
-                                ShaderResourceKind::Texture { .. } => &[
-                                    wgpu::BindGroupLayoutEntry {
-                                        binding: 0,
-                                        visibility: wgpu::ShaderStages::FRAGMENT,
-                                        ty: wgpu::BindingType::Texture {
-                                            sample_type: wgpu::TextureSampleType::Float {
-                                                filterable: true,
-                                            },
-                                            view_dimension: wgpu::TextureViewDimension::D2,
-                                            multisampled: false,
-                                        },
-                                        count: None,
-                                    },
-                                    wgpu::BindGroupLayoutEntry {
-                                        binding: 1,
-                                        visibility: wgpu::ShaderStages::FRAGMENT,
-                                        ty: wgpu::BindingType::Sampler(
-                                            wgpu::SamplerBindingType::Filtering,
-                                        ),
-                                        count: None,
-                                    },
-                                ],
-                                ShaderResourceKind::PropertyGroup(ref properties) => {
-                                    let entries: Vec<wgpu::BindGroupLayoutEntry> = properties.iter().enumerate().map(|(i, property)| {
-                                        wgpu::BindGroupLayoutEntry {
-                                            binding: i as u32,
-                                            visibility: ,
-                                            ty: todo!(),
-                                            count: todo!(),
-                                        }
-                                }).collect();
-                                    wgpu::BindGroupLayoutEntry {
-                                    },
-                                ],
-                            },
-                        });
+        let mut texture_entries = Vec::new();
+        let mut property_group_entries = Vec::new();
+        for resource in resources {
+            match resource.kind {
+                ShaderResourceKind::Texture { kind, fallback } => {
+                    let texture_entry = wgpu::BindGroupLayoutEntry {
+                        binding: 2 * resource.binding as u32,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                            view_dimension: wgpu::TextureViewDimension::D2,
+                            multisampled: false,
+                        },
+                        count: None,
+                    };
 
-                bind_group_layout
-            })
-            .collect();
+                    let sampler_entry = wgpu::BindGroupLayoutEntry {
+                        binding: 2 * resource.binding as u32 + 1,
+                        visibility: wgpu::ShaderStages::FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    };
+
+                    texture_entries.push(texture_entry);
+                    texture_entries.push(sampler_entry);
+                }
+                ShaderResourceKind::PropertyGroup(_) => {
+                    let property_group_entry = wgpu::BindGroupLayoutEntry {
+                        binding: resource.binding as u32,
+                        visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    };
+
+                    property_group_entries.push(property_group_entry);
+                }
+            }
+        }
+
+        let texture_bind_group_layout =
+            server
+                .device
+                .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                    label: None,
+                    entries: &texture_entries,
+                });
+
+        let property_group_bind_group_layout =
+            server
+                .device
+                .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                    label: None,
+                    entries: &property_group_entries,
+                });
 
         let layout = server
             .device
             .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: None,
-                bind_group_layouts: todo!(),
-                push_constant_ranges: todo!(),
+                bind_group_layouts: &[
+                    &texture_bind_group_layout,
+                    &property_group_bind_group_layout,
+                ],
+                push_constant_ranges: &[],
             });
+
         let pipeline = server
             .device
             .create_render_pipeline(&wgpu::RenderPipelineDescriptor {
                 label: None,
-                layout: todo!(),
-                vertex: todo!(),
+                layout: Some(&layout),
+                vertex: wgpu::VertexState {
+                    module: &shader_module,
+                    entry_point: None,
+                    compilation_options: Default::default(),
+                    buffers: todo!(),
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader_module,
+                    entry_point: None,
+                    compilation_options: Default::default(),
+                    targets: todo!(),
+                }),
                 primitive: todo!(),
                 depth_stencil: todo!(),
                 multisample: todo!(),
-                fragment: todo!(),
                 multiview: todo!(),
                 cache: todo!(),
             });
+
+        Ok(Self { pipeline })
     }
 }
 
